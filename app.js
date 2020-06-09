8c524465ebfdce3b8e883c199a588045b3c8a2b8
Nevin <39220032+TypicalNevin@users.noreply.github.com>
2020-06-09 20:41:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,12 @@ const getWeatherData = (latitude, longitude) => {
 // TODO : rewrite this as a promise
 const getWeatherData = (latitude, longitude) => {
   weather.setCoordinate(latitude, longitude);
-  weather.getTemperature().then((temp) => {
-    console.log(temp);
+  var hi = (weather.getTemperature = (err, temp) => {
+    if (err) throw err;
+    else {
+      callback(temp);
+    }
   });
+  console.log(hi);
   io.emit("server send weather data", this.temp);
 };
